06834381fdb3e14f89318c17d467b8801983befc
anshbadoni30 <b.anshbadoni@gmail.com>
2025-04-20 22:14:50 +0530

    changes in main.py, config.yaml, configuration.py & config_entity, creation of params.yaml,02_prepare_base_model.ipynb, prepare_base_model.py and state_02_prepare_base_model.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainigPipeline
 from cnnClassifier.logger import logging
 
 STAGE_NAME = "Data Ingestion stage"
@@ -8,5 +9,19 @@ try:
     obj=DataIngestionTrainingPipeline()
     obj.main()
     logging.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    raise e
+
+
+
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logging.info(f"*****************")
+    logging.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    obj = PrepareBaseModelTrainigPipeline()
+    obj.main()
+    logging.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx=====================x")
 except Exception as e:
     raise e
