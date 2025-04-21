375fcaacb625f04a61cdcfda72f48de38e7297a8
anshbadoni30 <b.anshbadoni@gmail.com>
2025-04-21 15:55:17 +0530

    changes in main.py, config_entity, configuration.py and creation of scores.json, 05_model_evaluation.ipynb, evaluation.py, stage_04_evaluation.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainigPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 from cnnClassifier.logger import logging
 
 STAGE_NAME = "Data Ingestion stage"
@@ -38,4 +39,18 @@ try:
     model_trainer.main()
     logging.info(f">>>>>>>>>>>>stage {STAGE_NAME} completed <<<<<<<<\n\nx==============x")
 except Exception as e:
+    raise e
+
+
+
+STAGE_NAME= "Evaluation stage"
+
+try:
+    logging.info(f"*******************")
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
     raise e
