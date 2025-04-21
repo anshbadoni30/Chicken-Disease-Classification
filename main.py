ab2f5a685ebb7e7d7e539a9cb65c9f266d181217
anshbadoni30 <b.anshbadoni@gmail.com>
2025-04-21 14:05:30 +0530

    changes in main.py, config_entity, configuration.py,config.yaml and creation of 04_trainig.ipynb, trainig.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainigPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 from cnnClassifier.logger import logging
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,5 +24,18 @@ try:
     obj = PrepareBaseModelTrainigPipeline()
     obj.main()
     logging.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx=====================x")
+except Exception as e:
+    raise e
+
+
+
+STAGE_NAME = "Training"
+
+try:
+    logging.info(f"******************************")
+    logging.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    model_trainer=ModelTrainingPipeline()
+    model_trainer.main()
+    logging.info(f">>>>>>>>>>>>stage {STAGE_NAME} completed <<<<<<<<\n\nx==============x")
 except Exception as e:
     raise e
---
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -0,0 +1,42 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+from cnnClassifier.entity.config_entity import PrepareCallbacksconfig
+
+class PrepareCallback:
+    def __init__(self, config: PrepareCallbacksconfig):
+        self.config=config
+
+    @property
+    def _create_tb_callbacks(self):
+        timestamp=time.strftime("%Y-%m-%d-%H-%M-%S")
+        tb_running_log_dir= os.path.join(
+            self.config.tensorboard_log_dir,
+            f"tb_logs_at{timestamp}", 
+        )
+        return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+    """
+    It creates TensorBoard callback object
+    Which will log all training progress to that folder, 
+    and TensorBoard will pick it up to display graphs and plots.
+    """
+    
+
+    @property
+    def _create_ckpt_callbacks(self):
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath = self.config.checkpoint_model_filepath,
+            save_best_only=True
+        )
+    #It will save the model to checkpoint_model_filepath only when validation loss improves (save_best_only=True).
+    
+
+    def get_tb_ckpt_callbacks(self):
+        return [
+            self._create_tb_callbacks,
+            self._create_ckpt_callbacks
+        ]
+    
+
